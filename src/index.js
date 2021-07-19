cf94255205993477bfc4d8f3dbc4f46cd271cf53
PrikhodkoAnna <prikhodkoannav@gmail.com>
2021-07-19 12:19:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+    let stack = [];
+    let flag = false;
+
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    else {
+        for (let i = 0; i < str.length; i++) {
+
+            let currentSymbol = str[i];
+            flag = false;
+            for (let j = 0; j < bracketsConfig.length; j++) {
+
+                if (bracketsConfig[j][0] === currentSymbol) {
+
+                    if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+
+                        if (stack.length < 1) {
+                            stack.push(currentSymbol);
+
+                        }
+                        else {
+                            if (stack[stack.length - 1] === currentSymbol) {
+                                stack.pop();
+
+                            }
+                            else {
+                                stack.push(currentSymbol);
+
+                            }
+                        }
+                    }
+
+                    else {
+                        stack.push(currentSymbol);
+
+                    }
+                    flag = true;
+                    continue;
+                }
+            }
+
+
+            if (flag === false) {
+                for (let j = 0; j < bracketsConfig.length; j++) {
+
+
+                    if (bracketsConfig[j][1] === currentSymbol) {
+
+                        if (stack.length < 1) {
+                            return false;
+                        }
+
+                        if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+                            stack.pop();
+                            continue;
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+
+
+
+
+    return stack.length === 0;
 }
